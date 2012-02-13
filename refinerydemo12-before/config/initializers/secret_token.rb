1cdfe08b9cc1eef3052b6ed903a9b87d7027f993
Josh Van Cleef <josh@joshvc.com>
2012-02-13 07:23:42 -0500

    Before (empty refinery app)

---
+++ refinerydemo12-before/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinerydemo12::Application.config.secret_token = 'bccbd9dd83dfe3bf9a13edf2b53c3557e5691698be8ebc3d8507a7eb840c3beeb7f50950b92102ccf48762deba85f4cc28cd64d341b621ff0168db25a17e9ceb'
